0ab0074727a26ca29713a999303d8d30fcc54ae9
pablo <perez51160900@hotmail.com>
2021-03-07 16:52:06 -0400

    subir fotos completado

--- tiendaFoto/api/controllers/AdminController.js
+++ tiendaFoto/api/controllers/AdminController.js
@@ -1,3 +1,6 @@
+
+const path = require('path'); 
+const fs = require('fs')
 module.exports = {
     inicio: async (peticion, respuesta)=>{
         respuesta.view('pages/admin/inicio_sesion')
@@ -29,13 +32,28 @@ module.exports = {
         peticion.session.carro = undefined;
         peticion.session.lista =  undefined;
         peticion.session.admin = undefined;
-        peticion.addFlash('mensaje', 'Sesión finalizada')
+        peticion.addFlash('men', 'Sesión finalizada')
         return respuesta.redirect("/");
       },
       crearFoto: async (peticion, respuesta)=>{
           respuesta.view('pages/admin/agregar_foto')
       }, 
       procesarFoto: async (peticion, respuesta)=>{
-          
+          let foto = await Foto.create({
+              precio: peticion.body.precio, 
+              activa: false
+          }).fetch(); 
+          peticion.file('foto').upload({}, async(error, archivos)=>{
+              if(archivos && archivos[0]){
+                  let upload_path = archivos[0].fd;
+                  let ext = path.extname(upload_path); 
+                  await fs.createReadStream(upload_path).pipe(fs.createWriteStream(path.resolve(sails.config.appPath,`assets/images/${foto.id}${ext}`)))
+                  await Foto.update({id:foto.id}, {contenido: `/images/${foto.id}${ext}`, activa:true})
+                  peticion.addFlash('men', 'Foto Agregada')
+                  return respuesta.redirect("/admin/index");
+              }
+                  peticion.addFlash('men', 'No llegaron archivos')
+                  return respuesta.redirect("/admin/index");
+          })
       }
 }
